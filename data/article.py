148270bcd8a17fe174bb807061a6f6074b702d4f
rhbrunetto <rhbrunetto>
2018-06-30 17:24:58 -0300

    Iniciando implementação

---
+++ data/article.py
@@ -0,0 +1,7 @@
+
+class Article:
+  """Represents an Article and its structure."""
+  def __init__(self, header, content, references):
+      self.header = header                        # Header
+      self.content = content                      # Content
+      self.references = references                # List of Reference
